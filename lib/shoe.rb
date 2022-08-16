2d362b7728d28f78b23a9614f9f868aebe52cf44
TONNYCHWEYA <chweyatonny@gmail.com>
2022-08-16 15:01:59 +0300

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :size
+
+    
+
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        @condition = "new"
+       puts "Your shoe is as good as new!"
+       
+    end
+end
